48eca336fd336eba1dd96f159adcb229a48f3cb6
jose-016al <jose_016al@outlook.com>
2023-10-11 12:31:41 +0200

    atenea

---
+++ HE/basico/descifrar.js
@@ -0,0 +1,3 @@
+var cadenaCodificada = "UmVjdWVyZGEgcXVlIGN1YW5kbyBjb2RpZmljYXMgYWxnbyBlbiBiYXNlNjQgTk8gbG8gZXN0w6FzIGNpZnJhbmRvLCBzaW5vIHF1ZSBzaW1wbGVtZW50ZSBsbyBlc3TDoXMgY29kaWZpY2FuZG8uDQoNCkxhIGNvbnRyYXNlw7FhIHBhcmEgc3VwZXJhciBlc3RlIHJldG8gZXM6IHJlY3VlcmRhcXVlYmFzZTY0Tk9lc2NpZnJhcg0KCg==";
+var cadenaDecodificada = atob(cadenaCodificada);
+console.log(cadenaDecodificada); // Esto imprimirá "Hola Mundo!"
