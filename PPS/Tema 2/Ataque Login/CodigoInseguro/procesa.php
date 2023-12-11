a76f636b391077f85d17b70482a0ef806ca489a8
jose-016al <jose_016al@outlook.com>
2023-12-11 18:29:57 +0100

    pps

--- PPS/Tema 2/Ataque Login/CodigoInseguro/procesa.php
+++ PPS/Tema 2/Ataque Login/CodigoInseguro/procesa.php
@@ -4,26 +4,40 @@
 
     if (isset($_SESSION['user'])) {
         header("location:menu.php");
-    } else {
-        header("location:index.php");
-    }
-
-    $user = $_POST['user'];
-    $password = md5($_POST['password']);
-
-    require_once("db.php");
-    $bd = Conectar::conexion();
-
-    $q = "SELECT * FROM users WHERE user ='" . $user . "' AND password ='" . $password . "'";
-    $results = $bd->query($q);
-    $datos = $results->fetch_assoc(); 
-
-    if (isset($user) && isset($password)) {
+    } elseif (isset($_POST['user']) && isset($_POST['password'])) {
+        $user = $_POST['user'];
+        $password = md5($_POST['password']);
+    
+        require_once("db.php");
+        $bd = Conectar::conexion();
+    
+            // Utilizar consultas preparadas para evitar inyección de SQL
+        $q = "SELECT * FROM users WHERE user = ? AND password = ?";
+        $stmt = $bd->prepare($q);
+    
+            // Vincular parámetros
+        $stmt->bind_param("ss", $user, $password);
+    
+            // Ejecutar la consulta
+        $stmt->execute();
+    
+            // Obtener resultados
+        $results = $stmt->get_result();
+        $datos = $results->fetch_assoc();
+    
+            // Cerrar la declaración
+        $stmt->close();
+    
         if ($datos) {
             $_SESSION['user'] = $datos;
             header("location:menu.php");
+            exit();
         } else {
             header("location: index.php?error=incorrect");
+            exit();
         }
+    } else {
+        header("location: index.php");
+        exit();
     }
 ?>
