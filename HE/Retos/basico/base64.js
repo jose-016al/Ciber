10ac22560de2292477c17b9ca26207924bc29523
jose-016al <jose_016al@outlook.com>
2024-02-09 21:01:50 +0100

    he

--- HE/Retos/basico/base64.js
+++
@@ -1,3 +0,0 @@
-var cadenaCodificada = "2d3f262032182d1b0316085c32023f3b";
-var cadenaDecodificada = atob(cadenaCodificada);
-console.log(cadenaDecodificada); // Esto imprimirá "Hola Mundo!"
--- HE/Retos/basico/ejer.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import sys
-
-if len(sys.argv) == 2:
-    hashmd5 = hashlib.md5()
-    text = sys.argv[1]
-    hashmd5.update(text.encode())
-    print ('flag{'+hashmd5.hexdigest()+'}')
